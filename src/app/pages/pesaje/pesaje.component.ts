b4891c5ef0737c3b7f87658de335a60380588293
unknown <iLalLoTiX>
2020-05-11 11:33:11 -0500

    Beta Modulo pesaje v1.0

--- src/app/pages/pesaje/pesaje.component.ts
+++ src/app/pages/pesaje/pesaje.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, NgForm, FormArray } from '@angular/forms';
-import { element } from 'protractor';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
+
+//servicios
 import { CajasService } from 'src/app/services/cajas.service';
-import { PdfMakeWrapper, Table } from 'pdfmake-wrapper';
+import { FrutasServices } from 'src/app/services/frutas.service';
+
+//plugins
+import { PdfMakeWrapper, Table} from 'pdfmake-wrapper';
+import swal from 'sweetalert';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 
 @Component({
@@ -15,8 +20,6 @@ export class PesajeComponent implements OnInit {
   forma: FormGroup;
   arreglo: FormGroup;
   public pedido;
-  public merma;
-  public kilosEx;
   B: number = 0;
   A: number = 0;
   producto: string;
@@ -27,14 +30,19 @@ export class PesajeComponent implements OnInit {
   totalBascula: number = 0;
   totalDestarado: number = 0;
   Falta: number = 0;
+  Sobra: number = 0;
 
   public llenarCajasSelect;
+  public llenarProductosSelect;
 
-  constructor(private fb: FormBuilder, public cs_: CajasService) { 
+  constructor(private fb: FormBuilder, public cs_: CajasService, public fs_: FrutasServices) {
 
     this.crearFormulario();
     this.crearArreglo();
     this.escucharPedido();
+    this.escucharKgEx();
+    this.escucharTarimas();
+    this.fs_.cargarTabla().subscribe(a => this.llenarProductosSelect = a);
     this.cs_.cargarCajas().subscribe(a => this.llenarCajasSelect = a);
   }
 
@@ -42,48 +50,74 @@ export class PesajeComponent implements OnInit {
   }
 
   imprimir(){
-    PdfMakeWrapper.setFonts(pdfFonts);
- 
-    const pdf = new PdfMakeWrapper();
-    
-    pdf.add('Tienda: ' + this.tienda);
-    pdf.add('producto: ' + this.producto);
-    pdf.add('Total a facturar: ' + this.totalFacturar);
-    pdf.add('Total Destarado: ' + this.totalDestarado);
-    pdf.add('Total de cajas: ' + this.totalCajas);
-    
-    
-    // let B = new Table([
-    //   [ 'Tarima', 'Cajas','Bascula','Destarado']
-    // ]).end;
-    // let C;
-    // pdf.add( B);
-    // this.tarimas.controls.forEach(tarima => {
-    //   C = new Table([
-    //     [ tarima.get('tarima').value, tarima.get('cajas').value, tarima.get('bascula').value, tarima.get('destarado').value]
-        
-    //   ]).widths([ 100 , 100 ]).end;
-    //   pdf.add( C);
-      
-    // });
-      
-      
-    pdf.create().open();
-    this.tarimas.controls.forEach(tarima => {
-      
-      this.cs_.prueba(tarima.get('tipo').value, tarima.get('cajas').value);
+    if(this.producto === undefined || this.tienda === undefined ){
+      swal("Falta especificar la tienda o el producto", {
+        icon: "warning",
+      });
+    }else{
+      PdfMakeWrapper.setFonts(pdfFonts);
+      const pdf = new PdfMakeWrapper();
+      pdf.add('Tienda: ' + this.tienda + '      Producto: ' + this.producto);
 
-    });
+      let C = new Table([
+        [ 'Tarima', 'Cajas','Bascula','Destarado']
+      ]).widths([ 100 , 100 , 100 , 100 ]).end;
+      pdf.add(C);
+      this.tarimas.controls.forEach(tarima => {
+        C = new Table([
+          [ tarima.get('tarima').value, tarima.get('cajas').value, tarima.get('bascula').value, tarima.get('destarado').value]
+        ]).widths([ 100 , 100 , 100 , 100 ]).end;
+        pdf.add( C);
+      });
+
+      C = new Table([
+        [ this.totalTarimas, this.totalCajas ,this.totalBascula, this.totalDestarado]
+      ]).widths([ 100 , 100 , 100 , 100 ]).background('#7cd5ff').end;
+      pdf.add( C);
+      pdf.add('Total a facturar: ' + this.totalFacturar);
+      pdf.create().open();
+    }
   }
 
-  reiniciarForm(){
+  altaCajas(){
+    swal({
+      title: "Estas seguro?",
+      text: "Esta accion modificara el inventario de cajas y reiniciará el formulario de pesaje",
+      icon: "warning",
+      buttons: ['Cancelar', true],
+      dangerMode: true,
+    })
+    .then((willDelete) => {
+      if (willDelete) {
+        this.tarimas.controls.forEach(tarima => {
+          if(tarima.get('cajas').value == null || tarima.get('tipos').value == null)
+          {
+            swal("Ingresa cuantas cajas y que tipo", {
+              icon: "warning",
+            });
+          }
+          else{
+            this.cs_.prueba(tarima.get('tipo').value, tarima.get('cajas').value);
+            swal("Listo!", {
+              icon: "success",
+            });
+          }
+        });
+      } else {
+        swal("No han habido cambios...");
+      }
+    });
+    
+  }
+
+  reiniciarForm(pedido: number, merma: number, kilosEx: number, total: number){
 
     this.forma.reset({
-      pedido  : this.forma.get('pedido').value,
-      kilosEx : this.forma.get('kilosEx').value,
-      merma   : this.forma.get('merma').value,
-      total   : this.forma.value.total
-      }
+      pedido,
+      kilosEx,
+      merma,
+      total
+      },{emitEvent: false}
     );
   }
 
@@ -117,7 +151,6 @@ export class PesajeComponent implements OnInit {
   }
 
   agregarTarima(){
-    
     this.tarimas.push(this.fb.group(
       {
         tarima: [],
@@ -128,6 +161,7 @@ export class PesajeComponent implements OnInit {
         destarado: [],
       }
     ));
+    this.escucharTarimas();
   }
 
   borrarTarima(i){
@@ -135,32 +169,31 @@ export class PesajeComponent implements OnInit {
   }
 
   escucharPedido() {
-    this.pedido = this.forma.valueChanges.subscribe( (forma: number) => {
-
-      
-        if(this.forma.get('pedido').dirty && !this.forma.get('pedido').touched){
-          forma['merma'] = forma['pedido'] * 0.05;
-          forma['kilosEx'] = forma['pedido'] / 100;
-          forma['total'] = forma['kilosEx'] + forma['pedido'] + forma['merma'];
-
-          this.reiniciarForm();
-
-        }
-
-        if((this.forma.get('merma').dirty && !this.forma.get('merma').touched) || (this.forma.get('kilosEx').dirty && !this.forma.get('kilosEx').touched)){
-          forma['total'] = forma['kilosEx'] + forma['pedido'] + forma['merma'];
-          
-          this.reiniciarForm();
-        }
-        
-        this.forma.patchValue({
-          merma  : forma['merma'],
-          kilosEx: forma['kilosEx'],
-          total  : forma['total'],
-        }, {emitEvent: false});
+    this.forma.get('pedido').valueChanges.subscribe( pedido => {
+      let merma = this.forma.get('merma').value;
+      let kilosEx = this.forma.get('kilosEx').value;
+      let total;
+      merma = pedido * 0.05;
+      kilosEx = pedido / 100;
+      total = kilosEx + pedido + merma;
+      this.reiniciarForm(pedido, merma, kilosEx, total);
+      this.operacionTarima();
     });
-    
-    
+  }
+
+  escucharKgEx(){
+    this.forma.get('kilosEx').valueChanges.subscribe( kilosEx => {
+      let pedido = this.forma.get('pedido').value;
+      let merma = this.forma.get('merma').value;
+      let total;
+      total = kilosEx + pedido + merma;
+      this.reiniciarForm(pedido, merma, kilosEx, total);
+      this.operacionTarima();
+    });
+  }
+
+  escucharTarimas(){
+    this.tarimas.controls.forEach( tarima => tarima.valueChanges.subscribe(a => this.operacionTarima()));
   }
 
   operacionTarima(){
@@ -169,11 +202,11 @@ export class PesajeComponent implements OnInit {
     this.totalBascula = 0;
     this.totalTarimas = 0;
     this.totalCajas = 0;
-    this.totalDestarado = 0
+    this.totalDestarado = 0;
     this.B = 0;
     this.A = 0;
     this.tarimas.controls.forEach(tarima => {
-      
+
       this.A = tarima.get('bascula').value -  ((tarima.get('cajas').value) * (tarima.get('peso').value));
       this.A = this.A - tarima.get('tarima').value;
       tarima.patchValue(
@@ -184,9 +217,11 @@ export class PesajeComponent implements OnInit {
       this.totalCajas = tarima.get('cajas').value + this.totalCajas;
       this.totalBascula = tarima.get('bascula').value + this.totalBascula;
       this.totalDestarado = tarima.get('destarado').value + this.totalDestarado;
-      this.totalFacturar = this.totalDestarado - ((this.totalDestarado)* 0.05) - this.forma.get('kilosEx').value;
+      this.totalFacturar = this.totalDestarado - ((this.totalDestarado) * 0.05) - this.forma.get('kilosEx').value;
+      this.totalFacturar.toFixed(2);
       this.Falta = this.totalFacturar - this.forma.get('pedido').value;
+      this.Sobra = Math.abs(this.Falta);
+      this.Sobra.toFixed(2);
     });
   }
-  
 }
